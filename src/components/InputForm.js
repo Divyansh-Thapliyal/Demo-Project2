d3b8bf86e877ec3cd4fd0be7d1dd8250a67c548a
Divyansh-Thapliyal <divyanshthapliyalwork007@gmail.com>
2023-07-22 17:24:30 +0530

    Form content added

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,10 +1,38 @@
 
+import {useState} from 'react';
+import './InputForm.css';
 
 const InputForm=()=>
 {
-    return <div>
 
-    </div>
+    const [userData,setUserData]=useState({userName:"",userAge:0});
+     
+    const stateHandler=(id,value)=>
+    {
+         setUserData((prevData)=>{
+            return{
+                ...prevData,
+                [id]:value
+            }
+         });
+    }
+    const formHandler=(event)=>
+    {
+        event.preventDefault();
+    }
+
+
+    return  <form onSubmit={formHandler} className='input_form'>
+<div className="mb-3">
+  <label htmlFor="formGroupExampleInput" className="form-label">Username</label>
+  <input type="text" className="form-control" onChange={(event)=>{stateHandler("userName",event.target.value)}} value={userData.userName} id="formGroupExampleInput" />
+</div>
+<div className="mb-3">
+  <label htmlFor="formGroupExampleInput2" className="form-label">Age(In years)</label>
+  <input type="Number" className="form-control" onChange={(event)=>{stateHandler("userAge",event.target.value)}} id="formGroupExampleInput2"/>
+</div>
+<button type="submit" className="btn btn-primary">Add User</button>
+</form>
 }
 
 export default InputForm;
